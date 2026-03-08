33b8f9f51a960b5fc887efeec70c4d6b6ccc9ace
jximel <167129101+jximel@users.noreply.github.com>
2026-03-08 16:40:37 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,7 +8,7 @@ def response_generator():
     [
       "Hello there! How can I assist you today?",
       "Hi, human! Is there anything I can help you with?",
-      "Do you need help?"
+      "Do you need help?",
     ]
 )
 for word in response.split():
